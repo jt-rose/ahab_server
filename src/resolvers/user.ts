9b163555956e91d25261dcdc088a1f382feec1e2
Jeffrey Rose <jtr219@outlook.com>
2021-01-21 00:28:08 -0500

    added login resolver

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import { Resolver, Ctx, Arg, Mutation, Field, InputType } from 'type-graphql'
+import { Resolver, Ctx, Arg, Mutation, Field, InputType, ObjectType } from 'type-graphql'
 import { User } from '../entities/USER'
 import { MyContext } from '../types'
 import argon2 from 'argon2'
@@ -9,6 +9,24 @@ class UsernamePasswordInput {
     username: string
     @Field()
     password: string
+}
+
+@ObjectType()// replace with union
+class UserResponse {
+    @Field(() => [FieldError], { nullable: true})
+    errors?: FieldError[]
+
+    @Field(() => User, { nullable: true})
+    user?: User
+}
+
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string
+
+    @Field()
+    message: string
 }
 
 @Resolver()
@@ -23,6 +41,40 @@ export class UserResolver {
                 const user = em.create(User, { username: options.username, password: hashedPassword})
                 await em.persistAndFlush(user)
                 return user
+            } catch(err) {
+                return err
+            }
+    }
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
+        @Ctx() { em }: MyContext
+        ): Promise<UserResponse> {
+            try {
+                const user = await em.findOne(User, {username: options.username})//.toLowercase()
+                if (!user) return {
+                    errors: [
+                        {
+                            field: 'username',
+                            message: 'no such user'
+                        }
+                    ]
+                }
+
+                const validPassword = await argon2.verify(user.password, options.password)
+                if (!validPassword) return {
+                    errors: [
+                        {
+                            field: 'password',
+                            message: 'Error: incorrect username/ password'
+                        }
+                    ]
+                }
+
+                return {
+                    user
+                }
             } catch(err) {
                 return err
             }
